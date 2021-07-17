53a84b6c23cb81ecbe5e95782f921b6eadae7674
Gregory Schier <gschier1990@gmail.com>
2021-07-16 21:47:43 -0700

    Remove day uniqueness from SID hash

--- backend/sid.go
+++ backend/sid.go
@@ -5,25 +5,24 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"time"
 )
 
 func GenerateSID(r *http.Request, siteID string) string {
+	sid := sha1.New()
+
+	sid.Write([]byte(Config.SessionSalt)) // Unique globally
+	sid.Write([]byte(siteID))             // Unique per site
+	sid.Write([]byte(GetIPAddress(r)))    // Unique per IP
+	sid.Write([]byte(r.UserAgent()))      // Unique per user-agent
+
+	return fmt.Sprintf("%x", sid.Sum(nil))
+}
+
+func GetIPAddress(r *http.Request) string {
 	ipAndPort := r.RemoteAddr
 	if forwardedFor := r.Header.Get("X-Forwarded-For"); forwardedFor != "" {
 		ipAndPort = forwardedFor
 	}
-	ip := strings.Split(ipAndPort, ":")[0]
 
-	n := time.Now()
-	dayStart := time.Date(n.Year(), n.Month(), n.Day(), 0, 0, 0, 0, n.Location())
-
-	sid := sha1.New()
-	sid.Write([]byte(Config.SessionSalt))            // Unique globally
-	sid.Write([]byte(siteID))                        // Unique per site
-	sid.Write([]byte(dayStart.Format(time.RFC3339))) // Unique per day
-	sid.Write([]byte(ip))                            // Unique per IP
-	sid.Write([]byte(r.UserAgent()))                 // Unique per user-agent
-
-	return fmt.Sprintf("%x", sid.Sum(nil))
+	return strings.Split(ipAndPort, ":")[0]
 }
