818e9bbe14f06b0524e4a3dcef739cc4b5394a1b
Gregory Schier <gschier1990@gmail.com>
2021-07-16 21:42:34 -0700

    Use screensize instead of document size

--- public/tracker.js
+++ public/tracker.js
@@ -1,46 +1,56 @@
-(function() {
-    function event(name) {
-        send('/event', [
-            { name: 'e', value: name },
-        ]);
+(function () {
+  // function event(name) {
+  //   send('/event', [{ name: 'e', value: name }]);
+  // }
+
+  function page() {
+    send('/page', [
+      {
+        name: 'h',
+        value: `${window.location.protocol}//${window.location.host}`,
+      },
+      { name: 'p', value: window.location.pathname },
+    ]);
+  }
+
+  function send(path, params) {
+    params.push({ name: 'id', value: website() });
+    params.push({
+      name: 'tz',
+      value: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    });
+    params.push({ name: 'xy', value: screensize() });
+    const qs = params
+      .map((v) => `${v.name}=${encodeURIComponent(v.value)}`)
+      .join('&');
+    const url = `${scriptOrigin()}/api${path}?${qs}`;
+    fetch(url, { mode: 'no-cors' }).catch((err) => console.log('Error:', err));
+  }
+
+  let _script = null;
+
+  function script() {
+    if (!_script) {
+      _script = document.querySelector('script[data-website]');
     }
+    return _script;
+  }
 
-    function page() {
-        send('/page', [
-            { name: 'h', value: `${window.location.protocol}//${window.location.host}` },
-            { name: 'p', value: window.location.pathname },
-        ]);
-    }
+  function screensize() {
+    const w = window.screen.w;
+    const h = window.screen.h;
+    return `${Math.round(w / 100) * 100}x${Math.round(h / 100) * 100}`;
+  }
 
-    function send(path, params) {
-        params.push({ name: 'id', value: website() });
-        params.push({ name: 'tz', value: Intl.DateTimeFormat().resolvedOptions().timeZone });
-        params.push({ name: 'xy', value: screensize() });
-        const qs = (params.map(v => `${v.name}=${encodeURIComponent(v.value)}`).join('&'));
-        const url = `${scriptOrigin()}/api${path}?${qs}`;
-        fetch(url, { mode: 'no-cors' }).catch(err => console.log('Error:', err));
-    }
+  function website() {
+    return script().getAttribute('data-website');
+  }
 
-    let _script = null;
+  function scriptOrigin() {
+    return script()
+      .getAttribute('src')
+      .match(/https:\/\/[^/]*/)[0];
+  }
 
-    function script() {
-        if (!_script) {
-            _script = document.querySelector('script[data-website]');
-        }
-        return _script;
-    }
-
-    function screensize() {
-        return `${Math.round(window.innerWidth / 100) * 100}x${Math.round(window.innerHeight / 100) * 100}`;
-    }
-
-    function website() {
-        return script().getAttribute('data-website');
-    }
-
-    function scriptOrigin() {
-        return script().getAttribute('src').match(/https:\/\/[^/]*/)[0];
-    }
-
-    page();
+  page();
 })();
