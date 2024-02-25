81b4ac5843ecdc12995379a8389624a7b297dd52
Gregory Schier <gschier1990@gmail.com>
2024-02-24 19:01:41 -0800

    Fix

--- src/pages/Site.tsx
+++ src/pages/Site.tsx
@@ -34,21 +34,22 @@ const Site: React.FC = () => {
     <>
       <Navbar website={website} />
       <VStack space={6} className="mx-4 my-6">
-        {currentUsers.data ||
-          summaryStats?.unique ||
-          (summaryStats?.total && (
-            <HStack space={3}>
-              <Card title="Live" className="w-full">
-                <HugeText>{currentUsers.data}</HugeText>
-              </Card>
-              <Card title="Visitors" className="w-full">
-                <HugeText>{summaryStats?.unique}</HugeText>
-              </Card>
-              <Card title="Views" className="w-full">
-                <HugeText>{summaryStats?.total}</HugeText>
-              </Card>
-            </HStack>
-          ))}
+        {(currentUsers.data ?? 0) +
+          (summaryStats?.unique ?? 0) +
+          (summaryStats?.total ?? 0) >
+          0 && (
+          <HStack space={3}>
+            <Card title="Live" className="w-full">
+              <HugeText>{currentUsers.data}</HugeText>
+            </Card>
+            <Card title="Visitors" className="w-full">
+              <HugeText>{summaryStats?.unique}</HugeText>
+            </Card>
+            <Card title="Views" className="w-full">
+              <HugeText>{summaryStats?.total}</HugeText>
+            </Card>
+          </HStack>
+        )}
 
         {rollups?.some((r) => r.total > 0) && (
           <div className="w-full h-64">
